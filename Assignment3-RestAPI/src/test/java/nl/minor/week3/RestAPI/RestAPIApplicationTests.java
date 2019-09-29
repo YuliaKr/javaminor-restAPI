82955b5d891e03cfae8daad8237a3a2ea8091523
Yulia <0937630@hr.nl>
2019-09-29 22:02:53 +0200

    initial-commit

--- Assignment3-RestAPI/src/test/java/nl/minor/week3/RestAPI/RestAPIApplicationTests.java
+++ Assignment3-RestAPI/src/test/java/nl/minor/week3/RestAPI/RestAPIApplicationTests.java
@@ -1,4 +1,4 @@
-package nl.javaminor.week3.RestEnControllers;
+package nl.minor.week3.RestAPI;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class RestEnControllersApplicationTests {
+public class RestAPIApplicationTests {
 
 	@Test
 	public void contextLoads() {
