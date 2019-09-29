d6f2567fd8ea181287256a58aca980d64b2357ea
Yulia <0937630@hr.nl>
2019-09-29 21:51:13 +0200

    initial-commit

---
+++ Assignment3-RestAPI/src/main/java/nl/javaminor/week3/RestEnControllers/RestEnControllersApplication.java
@@ -0,0 +1,13 @@
+package nl.javaminor.week3.RestEnControllers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestEnControllersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestEnControllersApplication.class, args);
+	}
+
+}
---
+++ Assignment3-RestAPI/src/test/java/nl/javaminor/week3/RestEnControllers/RestEnControllersApplicationTests.java
@@ -0,0 +1,16 @@
+package nl.javaminor.week3.RestEnControllers;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestEnControllersApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
